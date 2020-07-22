022b090f09cb336b160cbe98c8175303fae49048
leekyoungrok <kwtr2060@gmail.com>
2020-07-22 14:50:04 +0900

    20200722

--- STL/Code/예제코드/Chapter_3/set/set.cpp
+++ STL/Code/예제코드/Chapter_3/set/set.cpp
@@ -146,3 +146,24 @@ using namespace std;
 //	cout << (!s.key_comp()(30, 30) 
 //			&& !s.key_comp()(30, 30)) << endl; //같다(equivalence)
 //}
+
+void main()
+{
+	set<int, less<int> > s_less; // set<int> s와 같습니다.
+	set<int, greater<int> > s_greater; // 정렬 기준으로 greater<int> 조건자를 사용.
+	s_less.insert(50);
+	s_less.insert(80);
+	s_less.insert(40);
+	s_greater.insert(50);
+	s_greater.insert(80);
+	s_greater.insert(40);
+	set<int, less<int> >::key_compare l_cmp = s_less.key_comp();
+	cout << l_cmp(10, 20) << endl; // 10 < 20 연산
+	set<int, greater<int> >::key_compare g_cmp = s_greater.key_comp();
+	cout << g_cmp(10, 20) << endl; // 10 > 20 연산
+	//key_comp(), value_comp()의 타입을 문자열로 출력한다.
+	cout << "key_compare type: " << typeid(s_less.key_comp()).name() << endl;
+	cout << "key_compare type: " << typeid(s_greater.key_comp()).name() << endl;
+	cout << "value_compare type: " << typeid(s_less.value_comp()).name() << endl;
+	cout << "value_compare type: " << typeid(s_greater.value_comp()).name() << endl;
+}
